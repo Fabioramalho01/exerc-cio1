1606bf3cb209203262726a75a08b599e6e35f604
Fabioramalho01 <fabiougt@gmail.com>
2022-07-12 21:47:38 -0300

    Adicionando nova versao

---
+++ main.py
@@ -0,0 +1,20 @@
+nome: str = input (" Qual o seu nome? ")
+print ("olá", nome)
+data: str = input (" Qual o dia do seu nascimento? ")
+mes: str = input (" De que mês? ")
+ano = input (" Qual o ano do seu nascimento? ")
+n = int (2022)
+n1= int (ano)
+r = (n-n1)
+print ("Você tem", r, 'anos de idade, correto?')
+numero = input ("Digite um número")
+r1 = int (numero)
+r2 = n1 + r
+print ("A soma entre sua idade e o valor que você digitou é", r + r1)
+
+
+
+
+
+
+
